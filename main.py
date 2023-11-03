6fff50d9fe7d2c6fc0e978a551b1060064fb6f7c
ABinaya1999 <binayaparajuli17@gmail.com>
2023-11-03 08:58:23 +0545

    post

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
+class Post(BaseModel):
+    sport:str
+    best_player:str
+
 @app.get("/")
 async def root():
-    return {'message':'Hello binay'}
+    return {'message':'Hello binay raj parajuli'}
     
+@app.get("/home")
+def root(): 
+    return {'msg':'hello'}
+
+@app.post("/create_post")
+def create_post(post:Post):
+    print(post.sport)
+    return {"new_post":post} 
+                
